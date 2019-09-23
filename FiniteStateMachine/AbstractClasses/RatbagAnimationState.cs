7b6f9c5d0e3e53819236dab696e2ba6e89ca55a6
CaolanRadford <30577956+CaolanRadford@users.noreply.github.com>
2019-09-23 15:28:42 -0700

    Update RatbagAnimationState.cs

--- FiniteStateMachine/AbstractClasses/RatbagAnimationState.cs
+++ FiniteStateMachine/AbstractClasses/RatbagAnimationState.cs
@@ -1,7 +1,6 @@
 ﻿
 
 //this is an animation sub state for Ratbag, exposes OnInput methods
-//animation sub states are found within gameplay states when a gameplay state has more than one animation state
 
 using Components;
 using Controllers;
